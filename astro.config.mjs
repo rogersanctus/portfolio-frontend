6a09c8ad0d781fde0800a81b0fd968c6adf00e21
rogersanctus <rogersanctus@gmail.com>
2023-11-04 02:53:35 -0300

    Set server port to 5000

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  server: {
+    port: 5000
+  },
   integrations: [tailwind(), react()]
 })
