76c8c3322813d6b5482ac36f6bd4f22766e44131
rogersanctus <rogersanctus@gmail.com>
2023-11-07 00:26:33 -0300

    Adds astro SSR adapter for node server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +9,9 @@ export default defineConfig({
   server: {
     port: 5000
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: node({
+    mode: 'standalone'
+  })
 })
+
